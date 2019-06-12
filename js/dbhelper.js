a6ef87800c4bab7b682a84450fc6e16ab4f0d886
ApparaoSwamiKankipati <34737106+swamykankipati@users.noreply.github.com>
2019-06-12 18:19:34 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,9 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+   // return `http://localhost:${port}/data/restaurants.json`;
+    return `https://github.com/swamykankipati/Front-End-Nano-Degree-Project-Restaurant-Reviews-Stage-4/data/restaurants.json`
+
   }
 
   /**
