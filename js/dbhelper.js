c26c537078836ea6684cbd0456f533497d78ff6b
ApparaoSwamiKankipati <34737106+swamykankipati@users.noreply.github.com>
2019-06-12 18:56:19 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,17 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-   // return `http://localhost:${port}/data/restaurants.json`;
-   // return `https://github.com/swamykankipati/Front-End-Nano-Degree-Project-Restaurant-Reviews-Stage-4/data/restaurants.json`
-   //return  `./data/restaurants.json`;
-   //return  `https://github.com/swamykankipati/Front-End-Nano-Degree-Project-Restaurant-Reviews-Stage-4/blob/master/data/restaurants.json`;
-// if (Hosted or not ) {
-//     return `https://github.com/swamykankipati/Front-End-Nano-Degree-Project-Restaurant-Reviews-Stage-4/data/restaurants.json`
-//    } //return 'https://github.com/swamykankipati/Front-End-Nano-Degree-Project-Restaurant-Reviews-Stage-4'
-//    return `http://localhost:${port}/data/restaurants.json`;
- 
-//   }
+   // const port = 8000 // Change this to your server port
   return `./data/restaurants.json`
   }
 
